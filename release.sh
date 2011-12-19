27fd8a524b158e2ad64e8dadfe1cc4b40a6a6bb3
Erdinc Yilmazel <erdinc@yilmazel.com>
2011-12-19 10:19:59 -0500

    Jars needed for some extensions

--- release.sh
+++ release.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mvn clean source:jar javadoc:jar package
+
 target=$1
 rm -rf $target
 mkdir $target
@@ -18,6 +20,14 @@ mv cambridge-mvel/target/cambridge-mvel*.jar $target/
 mv cambridge-ognl/target/cambridge-ognl*-sources.jar $target/sources/
 mv cambridge-ognl/target/cambridge-ognl*-javadoc.jar $target/javadoc/
 mv cambridge-ognl/target/cambridge-ognl*.jar $target/
+
+mv cambridge-janino/target/cambridge-janino*-sources.jar $target/sources/
+mv cambridge-janino/target/cambridge-janino*-javadoc.jar $target/javadoc/
+mv cambridge-janino/target/cambridge-janino*.jar $target/
+
+mv cambridge-jexl/target/cambridge-jexl*-sources.jar $target/sources/
+mv cambridge-jexl/target/cambridge-jexl*-javadoc.jar $target/javadoc/
+mv cambridge-jexl/target/cambridge-jexl*.jar $target/
 
 mv cambridge-playframework/target/cambridge-playframework*-sources.jar $target/sources/
 mv cambridge-playframework/target/cambridge-playframework*-javadoc.jar $target/javadoc/
